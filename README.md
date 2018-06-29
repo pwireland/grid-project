ae263abcbb97d0d63c88f503d3ed1e91287864ab
Nathan Mouchiroud <nathan.mouchiroud@etu.univ-nantes.fr>
2018-06-29 16:10:50 +0100

    Messing around with data and colors

--- README.md
+++ README.md
@@ -2,26 +2,19 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
-## Development server
+## Requirements
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+- [Node.js and npm](https://nodejs.org/en/) (built with Node.js v8.x and npm v5.x)
+- [Angular CLI](https://angular.io/) (built with angular cli v6.x)
+- [MongoDB](https://www.mongodb.com/download-center) (built with Community edition v3.6)
+
+## Run
+
+Run `npm install` to install project dependencies.
+Then run `npm run build` and navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+
+*NB: Make sure the MongoDB server is running when launching the application. Refer to the [MongoDB Documentation](https://docs.mongodb.com/) if needed.*
 
 ## Code scaffolding
 
 Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,193 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { AgGridNg2 } from 'ag-grid-angular';
 import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
   @ViewChild('agGrid') agGrid: AgGridNg2;
 
-   // Define a users property to hold our user data
+  private columnDefs;
+  private defaultColDef;
+  // Define a users property to hold our user data
   users: Array<any>;
 
   // Create an instance of the DataService through dependency injection
   constructor(private _dataService: DataService) {
+    this.columnDefs = [
+      { headerName: 'Wafer no.', field: 'wafer_n', type: 'numericColumn', cellClass: 'cell-wafer-n' },
+      { headerName: 'LED No.', field: 'led_n', cellClass: 'cell-led-n' },
+      { headerName: 'Date', field: 'date', type: 'numericColumn' },
+      { headerName: 'Supplier', field: 'supplier', cellClass: 'cell-supplier' },
+      { headerName: 'Supplier PIN or Type', field: 'supplier_pin' },
+      { headerName: 'Scan or enter Lot/Batch No.', field: 'lot_n', cellClass: 'cell-lot-n' },
+      { headerName: 'Bin. Grade or Kit Number', field: 'bin_n', cellClass: 'cell-bin-n' },
+      { headerName: 'Qty on Wafer', field: 'qty_wafer', type: 'numericColumn' },
+      { headerName: 'Manufacturing date', field: 'manufacturing_date', type: 'numericColumn' },
+      { headerName: 'Test Current', field: 'test_current', type: 'numericColumn' },
+      { headerName: 'Min', field: 'min', type: 'numericColumn' },
+      { headerName: 'Ave.', field: 'average', type: 'numericColumn' },
+      { headerName: 'Max.', field: 'max', type: 'numericColumn' },
+      { headerName: 'Units', field: 'units' }
+    ];
+
+    // Default column options
+    this.defaultColDef = {
+      editable: true,
+      width: 96
+    };
+
     // Access the Data Service's getUsers() method
     this._dataService.getUsers()
-        .subscribe(res => this.users = res);
+      .subscribe(res => this.users = res);
   }
 
   private gridApi;
+  private gridColumnApi;
 
   title = 'Equipment DB app';
 
-  columnDefs = [
-    { headerName: 'Number', field: 'number', type: 'numericColumn', editable: true },
-    { headerName: 'Model', field: 'model', editable: true },
-    { headerName: 'Price', field: 'price', type: 'numericColumn', editable: true },
-  ];
-
-  rowData: any[];
+  private rowData: any[];
 
   ngOnInit() {
-    // this.rowData = this.http.get('https://api.myjson.com/bins/15psn9');
     this.rowData = [
-      {number: 1, model: 'Model 1', price: 20},
-      {number: 2, model: 'Model 2', price: 10},
-      {number: 3, model: 'Model 3', price: 50},
-      {number: 4, model: 'Model 4', price: 20},
-      {number: 5, model: 'Model 6', price: 40}
+      {
+        wafer_n: 4436, led_n: 'LED0020', date: '07/02/2018', supplier: 'EPIGAP', supplier_pin: 'ELOC-870-11',
+        lot_n: '2626H/003/6', bin_n: '', qty_wafer: 1412, manufacturing_date: '######', test_current: '20mA',
+        min: 1.31, average: 1.33, max: 1.33, units: 'V'
+      },
+      {
+        wafer_n: 4437, led_n: 'LED0020', date: '07/02/2018', supplier: 'EPIGAP', supplier_pin: 'ELOC-870-11',
+        lot_n: '2626H/0037a', bin_n: '', qty_wafer: 210, manufacturing_date: '######', test_current: '20mA',
+        min: 1.33, average: 1.33, max: 1.34, units: 'V'
+      },
+      {
+        wafer_n: 4438, led_n: 'LED0034', date: '08/02/2018', supplier: 'EPISTAR', supplier_pin: 'ES-SMHRPN42B',
+        lot_n: '22KY800900', bin_n: 'H15', qty_wafer: 927, manufacturing_date: '', test_current: '',
+        min: 2.00, average: 2.14, max: 2.40, units: ''
+      },
+      {
+        wafer_n: 4439, led_n: 'LED0034', date: '08/02/2018', supplier: 'EPISTAR', supplier_pin: 'ES-SMHRPN42B',
+        lot_n: '227KY800901', bin_n: 'H15', qty_wafer: 937, manufacturing_date: '', test_current: '',
+        min: 2.00, average: 2.13, max: 2.40, units: ''
+      },
+      {
+        wafer_n: 4440, led_n: 'LED0034', date: '08/02/2018', supplier: 'EPISTAR', supplier_pin: 'ES-SMHRPN42B',
+        lot_n: '22KY800902', bin_n: 'H15', qty_wafer: 876, manufacturing_date: '', test_current: '',
+        min: 2.00, average: 2.14, max: 2.40, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
+      {
+        wafer_n: 0, led_n: '', date: 0, supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+        manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
+      },
     ];
   }
 
   onGridReady(params) {
     this.gridApi = params.api;
+    this.gridColumnApi = params.columnApi;
+
+    // Automatically resize columns
+    this.autoSizeAll();
+    this.gridApi.setHeaderHeight(50);
+  }
+
+  // Automatically resize columns
+  autoSizeAll() {
+    const allColumnIds = [];
+    this.gridColumnApi.getAllColumns().forEach(function(column) {
+      allColumnIds.push(column.colId);
+    });
+    this.gridColumnApi.autoSizeColumns(allColumnIds);
   }
 
   getSelectedRows() {
     const selectedNodes = this.agGrid.api.getSelectedNodes();
     const selectedData = selectedNodes.map(node => node.data);
-    const selectedDataStringPresentation = selectedData.map(node => node.number + ' ' + node.model).join(', ');
+    const selectedDataStringPresentation = selectedData.map(node => node.wafer_n).join(', ');
     alert(`Selected nodes: ${selectedDataStringPresentation}`);
   }
 
   onAddRow() {
     const newItem = {
-      number: '',
-      model: '',
-      price: ''
+      wafer_n: 0, led_n: '', date: '', supplier: '', supplier_pin: '', lot_n: '', bin_n: '', qty_wafer: 0,
+      manufacturing_date: '', test_current: '', min: 0, average: 0, max: 0, units: ''
     };
     const res = this.gridApi.updateRowData({ add: [newItem] });
   }
